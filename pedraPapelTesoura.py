a1b1294820057179adb2913905e6bf8925398697
Caubi Neto <caubineto@outlook.com>
2023-09-03 14:19:30 -0300

    Atualizei o código em pedraPapelTesoura.py

--- pedraPapelTesoura.py
+++ pedraPapelTesoura.py
@@ -1,5 +1,6 @@
 import random
 
+# Definir as representações das mãos (pedra, papel, tesoura)
 pedra = '''
     _______
 ---'   ____)
@@ -29,17 +30,24 @@ tesoura = '''
 
 maos = [pedra, papel, tesoura]
 
+# Solicitar a escolha do usuário
 escolhaUsuario = int(input("Qual você escolhe? Digite 0 para Pedra, 1 para Papel ou 2 para Tesoura.\n"))
+
+# Imprimir a escolha do usuário
 print(maos[escolhaUsuario])
 
+# Gerar a escolha aleatória do computador
 escolhaPC = random.randint(0, 2)
+
+# Imprimir a escolha do computador
 print('O computador escolheu:')
 print(maos[escolhaPC])
 
+# Verificar o resultado do jogo
 if escolhaUsuario >= 3 or escolhaUsuario < 0:
     print('Você digitou um número inválido, você perdeu!')
 elif escolhaUsuario == 0 and escolhaPC == 2:
-    print('Voce venceu! Parabéns!')
+    print('Você venceu! Parabéns!')
 elif escolhaUsuario == 1 and escolhaPC == 0:
     print ('Você venceu! Parabéns!')
 elif escolhaUsuario == 2 and escolhaPC == 1:
